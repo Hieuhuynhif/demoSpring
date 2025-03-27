b2f1eafc7ce27dedaebe59b56e7f24e8bd2c3ba7
Hieuhuynh <hieuhuynhif@gmail.com>
2025-03-27 10:36:36 +0700

    demo

--- src/main/java/com/example/demo/demoController.java
+++ src/main/java/com/example/demo/demoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class demoController {
     @GetMapping("demo")
     public String demo() {
-        return "demso";
+        return "demo";
     }
 }
